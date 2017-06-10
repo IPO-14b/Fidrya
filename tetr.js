1eb08ba3d97d6e8873c11e063c0490149e3575ac
Hohol88 <EtniS1710@ya.ru>
2017-06-10 18:18:54 +0300

    Рисуем поле игры

--- tetr.js
+++ tetr.js
@@ -17,3 +17,18 @@ for (var i = 0; i < Tetris.pitch.height; i++) {
     Tetris.pitch.bricks[i][j] = 0;
   }
 }
+// Найдем на странице элемент, в котором будем рисовать тетрис
+var tetrisDom = document.getElementById(Tetris.config.pitchID);
+
+// Очистим на всякий случай его
+tetrisDom.innerHTML = '';
+// И пробежимся по массиву кирпичиков
+for (var i = 0; i < Tetris.pitch.bricks.length; i++) {
+  for (var j = 0; j < Tetris.pitch.bricks[i].length; j++) {
+    // Добавляя на игровое поле пустой или заполненный кирпичик
+    // (если в массиве кирпичиков 1, ставим заполненный кирпичик, если 0 - пустой)
+    tetrisDom.innerHTML += Tetris.pitch.bricks[i][j]
+                         ? Tetris.config.filledBrick
+                         : Tetris.config.freeBrick;
+  }
+}
